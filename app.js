e320dbcccf8caaecd052e2ffa8dc9041227d3b09
ojon1st <a.garba@hotmail.fr>
2018-04-20 18:58:54 +0100

    Launch nigerfoot app ...

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 
 var indexRouter = require('./routes/index');
@@ -16,11 +17,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.bodyParser());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+//app.use(express.bodyParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.methodOverride());
+app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
