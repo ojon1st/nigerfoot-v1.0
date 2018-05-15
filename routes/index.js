822c9044ebac89adbb7d3298423524b1754b0bb8
ojon1st <a.garba@hotmail.fr>
2018-05-15 21:22:48 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const User = require('../models/user');
 const router = express.Router();
-const user_controller = require('../controllers/userController.js');
+const user_controller = require('../controllers/usercontroller');
 
 router.get('/', (req, res) => {
     res.render('index', { user : req.user });
