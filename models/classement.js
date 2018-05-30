70073347460dbf8bd3f5bd09e77f146eff87d3c8
ojon1st <a.garba@hotmail.fr>
2018-05-31 00:12:02 +0100

    Add files via upload

--- models/classement.js
+++ models/classement.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var User = require('../models/user');
 var mongoose = require('mongoose');
 
@@ -13,20 +12,4 @@ var RankSchema = new Schema({
 });
 
 // Export model.
-=======
-var User = require('../models/user');
-var mongoose = require('mongoose');
-
-var Schema = mongoose.Schema;
-
-var RankSchema = new Schema({
-    rankstate: [{usrid:{type: Schema.ObjectId},
-                 usrpseudo:{type: String},
-                 usrscore:{type:Number, default:0},
-                 usrrank:{type:String}}],
-    rankaftergame:{type: Number,required: true}
-});
-
-// Export model.
->>>>>>> f58f6240db6c0608ecfc6abf65399674ea7c200f
 module.exports = mongoose.model('Rank', RankSchema);
